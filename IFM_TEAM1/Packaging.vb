5c7fa9a7c066775e0dc2cb65a8fca1f7d338082b
puseletso shantel <ps.marabele@icloud.com>
2022-09-08 00:54:30 +0200

    Update Packaging.vb
    just did the constructor and the property method

--- IFM_TEAM1/Packaging.vb
+++ IFM_TEAM1/Packaging.vb
@@ -5,5 +5,25 @@ Option Explicit On
 
 '222133448 , Marabele PS
 Public Class Packaging
+    'variable declaration 
+    Private _type As String
+
+
+    ' the constructor'
+    Public Sub New()
+
+    End Sub
+    ' the property method '
+    Public Property type As String
+        Get
+            Return _type
+        End Get
+        Set(value As String)
+            _type = value
+        End Set
+    End Property
+
+    ' the polymorphic function 
+
 
 End Class
